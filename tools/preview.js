a1440b364b464a85a73c6a9d671e94a3b7614e23
infNaN <38274826+infnan@users.noreply.github.com>
2021-01-17 01:03:42 +0800

    blog config
    Signed-off-by: infNaN <38274826+infnan@users.noreply.github.com>

--- tools/preview.js
+++ tools/preview.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-const conf = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf-8'));
+const conf = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf-8'), {
+    schema: yaml.DEFAULT_FULL_SCHEMA,
+});
 
 // 预览时不要压缩，浪费时间
 const minfiers = ['html_minifier', 'js_minifier', 'css_minifier', 'image_minifier'];
@@ -11,4 +13,6 @@ for (const minifier of minfiers) {
     }
 }
 
-fs.writeFileSync('./_config.tmp.yml', yaml.safeDump(conf));
+fs.writeFileSync('./_config.tmp.yml', yaml.safeDump(conf, {
+    schema: yaml.DEFAULT_FULL_SCHEMA,
+}));
